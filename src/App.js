f2c6a87d1f5b1701afb10d73fb265a59c4c685d8
tmosley03 <tlm3dev@gmail.com>
2018-08-01 13:09:31 -0500

    binding

--- src/App.js
+++ src/App.js
@@ -34,14 +34,26 @@ class App extends Component {
   }
 //================================  
   render() {
+    // this is an example of "inline styling". It is not true CSS and is scoped to this component.
+    const myStyle = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return <div className="App">
         <h1>Hello I'm a React.JS app</h1>
       <p>This is really working</p>
       {/* this syntax below does work, but it is ineffecient, so try to avoid it. use the 'bind' method as seen further below.*/}
-        <button onClick={() => this.switchNameHandler("DOLAMITE!!")} className="btn">
+      <button
+        style= {myStyle}
+        onClick={() => this.switchNameHandler("DOLAMITE!!")} className="btn">
           Switch Name
         </button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      {/* this is an example of 'binding'  */}
       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, "green lanterns")}
         changed={this.nameChangedHandler}>
           My hobbies: BJJ
